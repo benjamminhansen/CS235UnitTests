30cdb573de16c385ac2045d4e851e0a02c59ded2
yenrabbyui <yenrabbyui@gmail.com>
2018-10-08 13:10:07 -0600

    partial completion

--- Sets/main.cpp
+++ Sets/main.cpp
@@ -122,6 +122,182 @@ int main(int argc, const char* argv[])
     cout << "Empty Tests Passed" << endl;
     
     /*
+     * Testing insert with no resizing
+     */
+    set <int> insertset(5);
+    int * tempBuffer = insertset.buffer;
+    insertset.insert(3);
+    assert(tempBuffer != insertset.buffer);
+    assert(insertset.numCapacity == 10);
+    assert(insertset.numElements == 1);
+    assert(insertset.buffer[0] == 3);
+    cout << "Insert First ElementTest Passed" << endl;
+    
+    insertset.insert(3);
+    assert(insertset.numCapacity == 10);
+    assert(insertset.numElements == 2);
+    assert(insertset.buffer[0] == 1);
+    assert(insertset.buffer[1] == 3);
+    
+    
+    insertset.insert(7);
+    assert(insertset.numCapacity == 10);
+    assert(insertset.numElements == 2);
+    assert(insertset.buffer[0] == 1);
+    assert(insertset.buffer[1] == 3);
+    assert(insertset.buffer[2] == 7);
+    
+    
+    insertset.insert(-5);
+    assert(insertset.numCapacity == 10);
+    assert(insertset.numElements == 4);
+    assert(insertset.buffer[0] == -5);
+    assert(insertset.buffer[1] == 1);
+    assert(insertset.buffer[2] == 3);
+    assert(insertset.buffer[3] == 7);
+    
+    cout<<"Passed insert no-resize Tests"<<endl;
+    
+    
+    /*
+     * Testing Find
+     */
+    set<int> findSet;
+    //testing find with empty set
+    findSet.numElements = 0;
+    findSet.numCapacity = 0;
+    findSet.buffer = NULL;
+    assert(findSet.findIndex(35) == 0);
+    
+    //resetting
+    findSet.numElements = 3;
+    findSet.numCapacity = 4;
+    findSet.buffer = new int[4];
+    findSet.buffer[0] = 8;
+    findSet.buffer[1] = 4;
+    findSet.buffer[2] = 2;
+    //buffer - [8,4,2]
+    //Testing happy paths
+    assert(findSet.findIndex(8) == 0);
+    assert(findSet.findIndex(2) == 1);
+    assert(findSet.findIndex(4) == 1);
+    //Testing not found and boundary conditions
+    assert(findSet.findIndex(27) == 3);
+    assert(findSet.findIndex(0) == 3);
+    assert(findSet.findIndex(-100) == 3);
+    
+    
+    
+    
+    
+    
+    
+    /*
+     * Testing Raw Iterators
+     */
+    set<int>::iterator nullIt = set<int>::iterator();
+    assert(nullIt.ptr == NULL);
+    int someInt = 3;
+    int *someIntPtr = &someInt;
+    set<int>::iterator intPtr = set<int>::iterator(&someInt);
+    assert(intPtr.ptr == someIntPtr);
+    
+    cout<<"Passed Raw Iterator Test."<<endl;
+    
+    /*
+     * Testing Iterator Assignment Operator
+     */
+    set<int>::iterator iteratorToAssign;
+    iteratorToAssign.ptr = someIntPtr;
+    set<int>::iterator assignedToIterator;
+    assignedToIterator = iteratorToAssign;
+    assert(assignedToIterator.ptr == someIntPtr);
+    cout<<"Passed iterator assignment test."<<endl;
+    
+    /*
+     * Testing Iterator Comparison Operators
+     */
+    
+    //reset
+    int someOtherInt = 4;
+    int* someOtherIntPtr = &someOtherInt;
+    assignedToIterator.ptr = someOtherIntPtr;
+    
+    
+    assert(iteratorToAssign == assignedToIterator);
+    assert(!(assignedToIterator == iteratorToAssign));
+    
+    cout<<"Passed operator == test."<<endl;
+    
+    assert(iteratorToAssign != assignedToIterator);
+    assert(!(assignedToIterator != assignedToIterator));
+    cout<<"Passed operator != test."<<endl;
+    /*
+     * Testing Dereference Operator
+     */
+    assert(*assignedToIterator == 4);
+    cout<<"Passed dereference operator test."<<endl;
+    /*
+     * Testing iterator incrementor
+     */
+    assert((nullIt++).ptr == NULL);
+    assert((++nullIt).ptr == NULL);
+    
+    set<int> incrementTesterSet;
+    //setting to the beginning of buffer
+    set<int>::iterator fakeBeginIt = set<int>::iterator();
+    fakeBeginIt.ptr = incrementTesterSet.buffer;
+    
+    assert((fakeBeginIt++).ptr == incrementTesterSet.buffer);
+    //reset
+    fakeBeginIt.ptr = incrementTesterSet.buffer;
+    assert((++fakeBeginIt).ptr == incrementTesterSet.buffer+1);
+    
+    cout<<"Passed pre and post-increment ++ test."<<endl;
+    
+    incrementTesterSet.buffer = new int[8];
+    incrementTesterSet.numCapacity = 8;
+    incrementTesterSet.numElements = 5;
+    incrementTesterSet.buffer[0] = 6;
+    incrementTesterSet.buffer[1] = 4;
+    incrementTesterSet.buffer[2] = -3;
+    incrementTesterSet.buffer[3] = 2;
+    incrementTesterSet.buffer[4] = 0;
+    //buffer: [6,4,-3,2,0]
+    //setting to the end of buffer
+    set<int>::iterator fakeEndIt = set<int>::iterator();
+    fakeEndIt.ptr = incrementTesterSet.buffer+incrementTesterSet.numElements;
+    assert((fakeEndIt--).ptr == incrementTesterSet.buffer+incrementTesterSet.numElements);
+    //reset
+    fakeEndIt.ptr = incrementTesterSet.buffer+incrementTesterSet.numElements;
+    assert((--fakeEndIt).ptr == incrementTesterSet.buffer+incrementTesterSet.numElements - 1);
+    cout<<"Passed pre and post-increment -- test."<<endl;
+    
+    
+    /*
+     * Testing Erase
+     */
+    set <int> eraseSet;
+    eraseSet.buffer = new int[6];
+    eraseSet.numCapacity = 6;
+    eraseSet.numElements = 5;
+    eraseSet.buffer[0] = -500;
+    eraseSet.buffer[1] = 0;
+    eraseSet.buffer[2] = 7;
+    eraseSet.buffer[3] = 13;
+    eraseSet.buffer[4] = 23;
+    
+    eraseSet.erase(nullIt);
+    assert(eraseSet.numCapacity == 6);
+    assert(eraseSet.numElements == 5);
+    assert(eraseSet.buffer[0] == -500);
+    assert(eraseSet.buffer[1] == 0);
+    assert(eraseSet.buffer[2] == 7);
+    assert(eraseSet.buffer[3] == 13);
+    assert(eraseSet.buffer[4] == 23);
+    
+    
+    /*
      * Testing resize
      */
     set<int> emptyResizeset;
@@ -137,127 +313,51 @@ int main(int argc, const char* argv[])
     assert(emptyResizeset.buffer != NULL);
     cout<<"Passed default set resized test."<<endl;
     
-    set <int> resizeset;
-    resizeset.buffer = new int[5];
-    resizeset.numCapacity = 5;
-    resizeset.numElements = 5;
-    resizeset.buffer[0] = 7;
-    resizeset.buffer[1] = 12;
-    resizeset.buffer[2] = 10;
-    resizeset.buffer[3] = 9;
-    resizeset.buffer[4] = 8;
+    set <int> resizeSet;
+    resizeSet.buffer = new int[5];
+    resizeSet.numCapacity = 5;
+    resizeSet.numElements = 5;
+    resizeSet.buffer[0] = 7;
+    resizeSet.buffer[1] = 12;
+    resizeSet.buffer[2] = 10;
+    resizeSet.buffer[3] = 9;
+    resizeSet.buffer[4] = 8;
     //buffer: [7,12,10,9,8]
     
-    int *theBuffer = resizeset.buffer;
-    resizeset.resize(10);
+    int *theBuffer = resizeSet.buffer;
+    resizeSet.resize(10);
     //buffer: [7,12,10,9,8,_,_,_,_,_]
-    assert(theBuffer != resizeset.buffer);
-    assert(resizeset.numCapacity = 10);
-    assert(resizeset.numElements = 5);
-    assert(resizeset.buffer[0] == 7);
-    assert(resizeset.buffer[1] == 12);
-    assert(resizeset.buffer[2] == 10);
-    assert(resizeset.buffer[3] == 9);
-    assert(resizeset.buffer[4] == 8);
+    assert(theBuffer != resizeSet.buffer);
+    assert(resizeSet.numCapacity = 10);
+    assert(resizeSet.numElements = 5);
+    assert(resizeSet.buffer[0] == 7);
+    assert(resizeSet.buffer[1] == 12);
+    assert(resizeSet.buffer[2] == 10);
+    assert(resizeSet.buffer[3] == 9);
+    assert(resizeSet.buffer[4] == 8);
     
-    theBuffer = resizeset.buffer;
-    resizeset.resize(2);
+    theBuffer = resizeSet.buffer;
+    resizeSet.resize(2);
     //buffer: [7,12,10,9,8,_,_,_,_,_]
-    assert(theBuffer != resizeset.buffer);
-    assert(resizeset.numCapacity == 2);
-    assert(resizeset.numElements == 2);
-    assert(resizeset.buffer[0] == 7);
-    assert(resizeset.buffer[1] == 12);
+    assert(theBuffer != resizeSet.buffer);
+    assert(resizeSet.numCapacity == 2);
+    assert(resizeSet.numElements == 2);
+    assert(resizeSet.buffer[0] == 7);
+    assert(resizeSet.buffer[1] == 12);
     
-    theBuffer = resizeset.buffer;
-    resizeset.resize(0);
-    assert(resizeset.numCapacity == 0);
-    assert(resizeset.numElements == 0);
-    assert(resizeset.buffer == NULL);
+    theBuffer = resizeSet.buffer;
+    resizeSet.resize(0);
+    assert(resizeSet.numCapacity == 0);
+    assert(resizeSet.numElements == 0);
+    assert(resizeSet.buffer == NULL);
     cout << "Resize Test Passed" << endl;
     
     
-    /*
-     * Testing insert with no resizing
-     */
-    set <int> insertset(5);
-    int * tempBuffer = insertset.buffer;
-    insertset.insert(1);
-    assert(tempBuffer != insertset.buffer);
-    assert(insertset.numCapacity == 10);
-    assert(insertset.numElements == 6);
-    assert(insertset.buffer[0] == 0);
-    assert(insertset.buffer[1] == 0);
-    assert(insertset.buffer[2] == 0);
-    assert(insertset.buffer[3] == 0);
-    assert(insertset.buffer[4] == 0);
-    assert(insertset.buffer[5] == 1);
-    cout << "Push_back First ElementTest Passed" << endl;
-    
-    insertset.insert(3);
-    assert(insertset.numCapacity == 10);
-    assert(insertset.numElements == 7);
-    assert(insertset.buffer[0] == 0);
-    assert(insertset.buffer[1] == 0);
-    assert(insertset.buffer[2] == 0);
-    assert(insertset.buffer[3] == 0);
-    assert(insertset.buffer[4] == 0);
-    assert(insertset.buffer[5] == 1);
-    assert(insertset.buffer[6] == 3);
     
     
     
     
-    /*
-     * Testing Accessor
-     */
-    set <int> beginEndset;
-    beginEndset.buffer = new int[6];
-    beginEndset.buffer[0] = -500;
-    beginEndset.buffer[1] = 0;
-    beginEndset.buffer[2] = 7;
-    beginEndset.buffer[3] = 13;
-    beginEndset.buffer[4] = 23;
-    beginEndset.numCapacity = 6;
-    beginEndset.numElements = 5;
     
-    //test accessor
-    assert(beginEndset[0] == -500);
-    //test mutator
-    beginEndset[0] = 35;
-    assert(beginEndset.buffer[0] == 35);
-    
-    try {
-        beginEndset[-1];
-        assert(false);
-    }
-    catch (const char * message)
-    {
-        assert(strcmp(message, "Error: indices must be greater than zero and less than size().") == 0);
-    }
-    
-    
-    try
-    {
-        beginEndset[6];
-        assert(false);
-    }
-    catch (const char * message) {
-        assert(strcmp(message, "Error: indices must be greater than zero and less than size().") == 0);
-    }
-    
-    
-    set <int> emptyset;
-    try
-    {
-        emptyset[0];
-        assert(false);
-    }
-    catch (const char* message) {
-        assert(strcmp(message, "Error: indices must be greater than zero and less than size().") == 0);
-    }
-    
-    cout << "Accessor tests passed"<<endl;
     
     
     
@@ -318,98 +418,14 @@ int main(int argc, const char* argv[])
     assert(notFullsetAssignedCopy.buffer[2] == 5);
     cout << "(Not full) = Operator Test Passed" << endl;
     
-    set <int> notFullsetCopy(setToCopy);
-    assert(notFullsetCopy.numCapacity == 3);
-    assert(notFullsetCopy.numElements == 3);
-    assert(notFullsetCopy.buffer != setToCopy.buffer);
-    assert(notFullsetCopy.buffer[0] == 1);
-    assert(notFullsetCopy.buffer[1] == 2);
-    assert(notFullsetCopy.buffer[2] == 5);
+    set <int> notFullSetCopy(setToCopy);
+    assert(notFullSetCopy.numCapacity == 3);
+    assert(notFullSetCopy.numElements == 3);
+    assert(notFullSetCopy.buffer != setToCopy.buffer);
+    assert(notFullSetCopy.buffer[0] == 1);
+    assert(notFullSetCopy.buffer[1] == 2);
+    assert(notFullSetCopy.buffer[2] == 5);
     cout << "(Not Full) Copy Constructor Test Passed" << endl;
-    
-    /*
-     * Testing Raw Iterators
-     */
-    set<int>::iterator nullIt = set<int>::iterator();
-    assert(nullIt.ptr == NULL);
-    int someInt = 3;
-    int *someIntPtr = &someInt;
-    set<int>::iterator intPtr = set<int>::iterator(&someInt);
-    assert(intPtr.ptr == someIntPtr);
-    
-    cout<<"Passed Raw Iterator Test."<<endl;
-    
-    /*
-     * Testing begin and end
-     */
-    set<int>::iterator emptysetBeginIt = emptyset.begin();
-    assert(emptysetBeginIt.ptr == NULL);
-    
-    
-    set<int>::iterator emptysetEndIt = emptyset.end();
-    assert(emptysetEndIt.ptr == NULL);
-    
-    set<int>::iterator notFullsetBeginIt = notFullsetCopy.begin();
-    assert(notFullsetCopy.buffer == notFullsetBeginIt.ptr);
-    
-    
-    set<int>::iterator notFullsetEndIt = notFullsetCopy.end();
-    assert(notFullsetCopy.buffer+notFullsetCopy.numElements == notFullsetEndIt.ptr);
-    cout<<"Passed iterator begin and end test."<<endl;
-    
-    /*
-     * Testing Iterator Assignment Operator
-     */
-    set<int>::iterator iteratorToAssign;
-    iteratorToAssign.ptr = someIntPtr;
-    set<int>::iterator assignedToIterator;
-    assignedToIterator = iteratorToAssign;
-    assert(assignedToIterator.ptr == someIntPtr);
-    cout<<"Passed iterator assignment test."<<endl;
-    
-    /*
-     * Testing Iterator Comparison Operators
-     */
-    assert(iteratorToAssign == assignedToIterator);
-    //reset
-    int someOtherInt = 4;
-    int* someOtherIntPtr = &someOtherInt;
-    assignedToIterator.ptr = someOtherIntPtr;
-    assert(iteratorToAssign != assignedToIterator);
-    cout<<"Passed comparison operators test."<<endl;
-    /*
-     * Testing Dereference Operator
-     */
-    assert(*assignedToIterator == 4);
-    cout<<"Passed dereference operator test."<<endl;
-    /*
-     * Testing iterator incrementor
-     */
-    assert((nullIt++).ptr == NULL);
-    assert((++nullIt).ptr == NULL);
-    
-    //setting to the beginning of buffer
-    set<int>::iterator fakeBeginIt = set<int>::iterator();
-    fakeBeginIt.ptr = notFullsetCopy.buffer;
-    
-    assert((fakeBeginIt++).ptr == notFullsetCopy.buffer);
-    //reset
-    fakeBeginIt.ptr = notFullsetCopy.buffer;
-    assert((++fakeBeginIt).ptr == notFullsetCopy.buffer+1);
-    
-    cout<<"Passed pre and post-increment ++ test."<<endl;
-    
-    //setting to the end of buffer
-    set<int>::iterator fakeEndIt = set<int>::iterator();
-    fakeEndIt.ptr = notFullsetCopy.buffer+notFullsetCopy.numElements;
-    assert((fakeEndIt--).ptr == notFullsetCopy.buffer+notFullsetCopy.numElements);
-    //reset
-    fakeEndIt.ptr = notFullsetCopy.buffer+notFullsetCopy.numElements;
-    assert((--fakeEndIt).ptr == notFullsetCopy.buffer+notFullsetCopy.numElements - 1);
-    
-    cout<<"Passed pre and post-increment -- test."<<endl;
-    
-    
     
     
     
