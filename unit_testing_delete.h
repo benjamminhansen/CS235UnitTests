6aaa8f99a1f54bb9a9c16d447bf65f62e72077a8
yenrabbyui <yenrabbyui@gmail.com>
2018-10-20 11:30:08 -0600

    added comments

--- unit_testing_delete.h
+++ unit_testing_delete.h
@@ -10,8 +10,10 @@
 #define unit_testing_delete_h
 
 #ifdef UNIT_TESTING
-
+//This global int variable is used to track calls to delete for unit testing purposes only.
 int unit_testing_delete_call_counter = 0;
+//overloading the delete operator globally.
+//DON'T EDIT THIS CODE!! IF YOU DO, YOU WILL HATE LIFE.
 void operator delete(void * p)
 {
     free(p);
